5fd714f2bf97820cb142851995af25f976975bf7
cm23abr <charvimaurya0110@gmail.com>
2025-12-04 17:33:41 +0100

    commit

---
+++ api/main.py
@@ -0,0 +1,42 @@
+import joblib
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+app = FastAPI()
+
+try:
+    vectorizer = joblib.load('../models/tfidf_vectorizer.pkl')
+    model = joblib.load('../models/logistic_regression.pkl')
+except Exception as e:
+    raise RuntimeError(f'Model failed loading: {e}')
+
+
+class ReviewRequest(BaseModel):
+    review: str
+
+@app.get('/')
+def root():
+    return {'message': 'Hello World'};
+
+@app.post("/predict")
+def predict_sentiment(review: str):
+    try:
+        # Check for empty input
+        if not review or review.strip() == "":
+            raise HTTPException(status_code=400, detail="Review cannot be empty.")
+
+        # Convert text to vector
+        X = vectorizer.transform([review])
+
+        # Predict
+        pred = model.predict(X)[0]
+        sentiment = "Good" if pred == 2 else "Bad"
+
+        return {"sentiment": sentiment}
+
+    except HTTPException:
+        raise  # re-throw FastAPI errors
+
+    except Exception as e:
+        # Catch-all error
+        raise HTTPException(status_code=500, detail=f"Prediction failed: {str(e)}")
+
